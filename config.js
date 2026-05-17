21672a3f13d0359acecd1fc423d7dba6cbbe2b7a
jnnico <jnnico@users.noreply.github.com>
2026-05-17 02:46:30 +0200

    Connect Google Sheet voting endpoint

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Pega aqui la URL del despliegue de Apps Script terminado en /exec.
 // Ejemplo:
 // window.GALICIA_VOTES_API_URL = "https://script.google.com/macros/s/AKfycb.../exec";
-window.GALICIA_VOTES_API_URL = "";
+window.GALICIA_VOTES_API_URL = "https://script.google.com/macros/s/AKfycbxzwRWVaqtrfjLWChvlR-zEvoaVLZg0wwQrUxhJPm9aYPK1DQ5i3s_KeGHud2PtlhYrMA/exec";
